c8a8bc6263cf3358f0fcafc461abfc39f1eaccf7
lody ang <melodyang13@gmail.com>
2024-09-18 15:40:10 +0800

    some chagnes again

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
                 
             </div>
           
-          <div className='col-span-1 xl: row-span-3'>
+          <div className='col-span-1 xl:row-span-3'>
               <div className="grid-container flex flex-col items-center">
                   <img src="assets/grid2.png" alt='grid-2' className='w-full sm:w-[276px] h-fit object-contain'/>
                   <div>

ca21e94b7d98e15845a3a0dfc2d4a3c316c20751
lody ang <melodyang13@gmail.com>
2024-09-20 23:22:50 +0800

    added work experience section and edited content

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './sections/Navbar';
 import Hero from './sections/Hero';
 import About from './sections/About';
 import Projects from './sections/Projects';
+import Experience from './sections/Experience';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Hero />
         <About />
         <Projects/>
+        <Experience/>
       </main>
     
   );
--- src/constants/index.js
+++ src/constants/index.js
@@ -21,41 +21,6 @@ export const navLinks = [
     },
   ];
   
-  export const clientReviews = [
-    {
-      id: 1,
-      name: 'Emily Johnson',
-      position: 'Marketing Director at GreenLeaf',
-      img: 'assets/review1.png',
-      review:
-        'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
-    },
-    {
-      id: 2,
-      name: 'Mark Rogers',
-      position: 'Founder of TechGear Shop',
-      img: 'assets/review2.png',
-      review:
-        'Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
-    },
-    {
-      id: 3,
-      name: 'John Dohsas',
-      position: 'Project Manager at UrbanTech ',
-      img: 'assets/review3.png',
-      review:
-        'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
-    },
-    {
-      id: 4,
-      name: 'Ether Smith',
-      position: 'CEO of BrightStar Enterprises',
-      img: 'assets/review4.png',
-      review:
-        'Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
-    },
-  ];
-  
   export const myProjects = [
     {
       title: 'Pathora.AI - Capstone Project',
@@ -92,13 +57,18 @@ export const navLinks = [
           name: 'MonogoDB',
           path: 'assets/framer.png',
         },
+        {
+          id: 5,
+          name: 'Big Three',
+          path: 'assets/framer.png',
+        },
       ],
     },
     {
-      title: 'LiveDoc - Real-Time Google Docs Clone',
-      desc: 'LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.',
+      title: 'Web Technologies: phHits',
+      desc: 'Video Streaming Website | Front End Developer',
       subdesc:
-        'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
+        'Made use of Figma to design the interface and used HTML, CSS , and JS to create the front-end module',
       href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
       texture: 'textures/project/project2.mp4',
       logo: 'assets/project-logo2.png',
@@ -132,10 +102,10 @@ export const navLinks = [
       ],
     },
     {
-      title: 'CarePulse - Health Management System',
-      desc: 'An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.',
+      title: 'Wordy Game',
+      desc: 'Java Application | Developer and UI Designer',
       subdesc:
-        'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
+        'A multi-player spelling game, where the player with the longest word wins ',
       href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
       texture: 'textures/project/project3.mp4',
       logo: 'assets/project-logo3.png',
@@ -171,10 +141,10 @@ export const navLinks = [
       ],
     },
     {
-      title: 'Horizon - Online Banking Platform',
-      desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
+      title: 'Lucida',
+      desc: ' Java Application | Developer',
       subdesc:
-        'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
+        'An employee attendance monitoring system that tracks the status of employees in real-time',
       href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
       texture: 'textures/project/project4.mp4',
       logo: 'assets/project-logo4.png',
@@ -260,29 +230,20 @@ export const navLinks = [
   export const workExperiences = [
     {
       id: 1,
-      name: 'Framer',
-      pos: 'Lead Web Developer',
-      duration: '2022 - Present',
-      title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
+      name: 'TJ Broz Laundry Shop',
+      pos: 'Front End Developer',
+      duration: 'Jun - Aug 2024',
+      title: "Loyalty Point System Web Application that allows customers to collect stickers for a free laundry service. Made use of React, HTML, CSS, and JavaScript to create a functional and user-friendly interface.",
       icon: 'assets/framer.svg',
       animation: 'victory',
     },
     {
       id: 2,
-      name: 'Figma',
-      pos: 'Web Developer',
-      duration: '2020 - 2022',
-      title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
+      name: 'G&C Real Estate Services',
+      pos: 'UI Designer',
+      duration: 'Jun - Jul 2024',
+      title: "Real Estate Services Web App Design. Made use of Figma to create a visually appealing and user-friendly interface.",
       icon: 'assets/figma.svg',
-      animation: 'clapping',
-    },
-    {
-      id: 3,
-      name: 'Notion',
-      pos: 'Junior Web Developer',
-      duration: '2019 - 2020',
-      title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
-      icon: 'assets/notion.svg',
       animation: 'salute',
     },
   ];

e4a4f7418a6bed981b64043816501d6ae21b51d4
lody ang <melodyang13@gmail.com>
2024-09-26 00:51:07 +0800

    added githublink

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
             <p></p>
         </div>
         <div className='flex gap-3'>
-            <a href=''>
+            <a href='https://github.com/melodyang13'>
             <div className='social-icon'>
                 <img src='assets/github.svg' alt='github' className='w-1/2 h-1/2'/>
             </div>
